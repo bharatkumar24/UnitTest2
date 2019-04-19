1b99852e53801d31fe7dc47255038a8659fa66c9
bharat <bharat@DISHANT>
2019-04-19 14:06:40 +0530

    Add project files.

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestProject;
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(UnitTestClass.IsTriangle(7));
+        }
+    }
+}
---
+++ UnitTestProject/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject
+{
+    public class UnitTestClass
+    {
+        public static bool IsTriangle(int a)
+        {
+            if (a== 0)
+                return true;
+            else
+                return false;
+
+        }
+    }
+}
